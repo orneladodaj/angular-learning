5b72e370839ad79499a6c447517b9f6df663079d
Ornela <orneladodaj.work@gmail.com>
2025-03-09 00:02:05 +0100

    added new methods

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,34 @@
-import { Component, Input, input, computed ,Output, EventEmitter} from '@angular/core';
-
+import {
+  Component,
+  Input,
+  input,
+  computed,
+  Output,
+  EventEmitter,
+  output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-   //@Input({required:true}) avatar!:string;
-   //@Input({required:true}) name!:string;
-
-   avatar = input.required<string>();
-   name = input.required<string>();
-   @Output() select = new EventEmitter();
-   get imagePath(){
-    return  '../assets/users/' + this.avatar;
-   }
-    
-
-   onSelectUser (){
-    this.select.emit();
-   }
-
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+ // select = output<string>(); //output is a function , we dont have to use event emitter
+  get imagePath() {
+    return '../assets/users/' + this.avatar;
   }
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+}
+// avatar = input.required<string>();
+//name = input.required<string>();
+//@Output() select = new EventEmitter();
+//these are old ways to do the things
