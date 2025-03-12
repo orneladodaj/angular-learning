c11cd099835435a0b1937bf0ada8aaca6f2b1f89
Ornela <orneladodaj.work@gmail.com>
2025-03-12 18:23:22 +0100

    added new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,17 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './user/dummy-users';
 import { TaskComponent } from "./task/task.component";
+import { NgFor , NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TaskComponent], // Importing the standalone component
+  imports: [HeaderComponent, UserComponent, TaskComponent, NgFor,NgIf], // Importing the standalone component
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'ul';
+  selectedUserId?: string;
 
   get selectedUser(){
     return this.users.find((user)=> user.id === this.selectedUserId)!;
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,10 +1,11 @@
 import { Component , Input, EventEmitter , Output } from '@angular/core';
 import { UserComponent } from '../user/user.component';
+import { UserTaskComponent } from "./user-task/user-task.component";
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [UserTaskComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
