b6a10639b282273611117f1a2839120adadc0f7b
Ornela <orneladodaj.work@gmail.com>
2025-03-18 12:35:01 +0100

    worked with services

--- src/app/task/tasks.service.ts
+++ src/app/task/tasks.service.ts
@@ -29,6 +29,14 @@ import { NewTaskData } from "./user-task/task.model";
       },
   ];
 
+  constructor(){
+    const tasks = localStorage.getItem('tasks');
+
+    if(tasks){
+        this.tasks = JSON.parse(tasks);
+    }
+  }
+
    getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
    }
@@ -39,11 +47,15 @@ import { NewTaskData } from "./user-task/task.model";
         title: taskData.title,
         summary: taskData.summary,
         dueDate: taskData.date
-       })
+       });
+       this.saveTasks();
     
     }
     removeTask(id: string){
         this.tasks = this.tasks.filter((task)=> task.id !==id);
+        this.saveTasks();
     }
-   
+   private saveTasks(){
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+   }
 }
